51f875b6e191bc228ca61d313ae234ec3214e031
monicaanavap <31547577+monicaanavap@users.noreply.github.com>
2021-04-26 21:21:26 -0500

    pagina home

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,28 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <!-- <h1>{{ msg }}</h1> -->
+    <img alt="banner" src="../assets/private-label-candles.png">
+    <h1>Sobre Nosotros</h1>
+    <section> 
+      <p> Fundamos Imperial Candles, con una misión: ser un(a) Tienda de velas de alta calidad que haga a nuestros consumidores parte de una experiencia superior. Nuestra pasión por la excelencia nos condujo a materializar esta misión, siendo ésta la parte fundamental que nos ha impulsado a seguir adelante. Nos enorgullecemos de ofrecer una experiencia de compra superior. Descubre más sobre nuestros productos. </p>
+      <img alt="candles2" src="../assets/candles3.png">
+    </section>
+    <section class="box">
+      <h2> Preguntas frecuentes </h2>
+      <section class= "boxes">
+        <section class="minibox">
+          <h3> Envios? </h3>
+          <p> Por el momento </p>
+        </section>
+        <section class="minibox">
+          <h3> Devoluciones? </h3>
+          <p> Fun fundamenllecemos deuestros productos. </p>
+        </section>
+      </section>
+    </section> 
+    <footer class="footer-home">
+      <h1> Contactanos </h1>
+    </footer>
   </div>
 </template>
 
@@ -41,8 +37,20 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.banner{
+  max-width: 100%;
+  height: auto;
+  width: auto;
+}
+img{
+  margin: 15px 15px 15px 15px; 
+}
+p{
+  margin: 30px 30px 30px 30px; 
+}
 h3 {
-  margin: 40px 0 0;
+  text-align: center;
+  color: #4F2310;
 }
 ul {
   list-style-type: none;
@@ -54,5 +62,47 @@ li {
 }
 a {
   color: #42b983;
+}
+h1{
+  text-align: left;
+  margin-left: 30px;
+}
+h2{
+  text-align: left;
+  color: #4F2310;
+  margin-left: 20px; 
+}
+section{
+   background-color: #EFE9D8;
+  border: 3px; 
+  color: #4F2310;
+  display: flex;
+  flex-direction: row; 
+  margin: 30px 30px 30px 30px; 
+}
+.box{
+  background-color: #EFE9D8;
+  border: 3px; 
+  color: #4F2310;
+  display: flex;
+  flex-direction: column; 
+  margin: 30px 30px 30px 30px; 
+}
+.boxes{
+  display: flex;
+  flex-direction: row; 
+  justify-content: space-evenly;
+}
+.minibox{
+  background-color: #E3B575;
+  border: 3px; 
+  margin: 10px 20px 10px 20px;
+  color: #4F2310;
+  display: flex;
+  flex-direction: column; 
+}
+footer{
+  background-color: #4F2310;
+  color: white; 
 }
 </style>
